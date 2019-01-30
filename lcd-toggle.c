3a36181499cb37ec35db531f0a67474c779a2a99
Federico Stra <stra.federico@gmail.com>
2019-01-30 01:49:45 +0100

    Refactor
    Simplify error handling.

--- lcd-toggle.c
+++ lcd-toggle.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <error.h>
+
+const char lcd_path[] = "/sys/class/backlight/intel_backlight/bl_power";
 
 int main() {
 	char state;
 
-	FILE *lcd_file = fopen("/sys/class/backlight/intel_backlight/bl_power", "r+");
-	if (lcd_file == NULL) {
-		perror("Opening");
-		exit(EXIT_FAILURE);
-	}
+	FILE *lcd_file = fopen(lcd_path, "r+");
+	if (lcd_file == NULL)
+		error(EXIT_FAILURE, errno, "Write %s", lcd_path);
 
 	int nread = fread(&state, sizeof(char), 1, lcd_file);
-	if (nread != 1) {
-		perror("Reading");
-		exit(EXIT_FAILURE);
-	}
+	if (nread != 1)
+		error(EXIT_FAILURE, errno, "Read %s", lcd_path);
 
 	/* printf("The current state is %c\n", buf[0]); */
 	
@@ -26,10 +26,8 @@ int main() {
 	fseek(lcd_file, 0L, SEEK_SET);
 
 	int nwritten = fwrite(&state, sizeof(char), 1, lcd_file);
-	if (nwritten != 1) {
-		perror("Writing");
-		exit(EXIT_FAILURE);
-	}
+	if (nwritten != 1)
+		error(EXIT_FAILURE, errno, "Write %s", lcd_path);
 
 	fclose(lcd_file);
 }
